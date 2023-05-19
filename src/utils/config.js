7d24c30b02a1a1b03e0ded1f681daf180843bda5
AlexanderMolokanov <91531787+AlexanderMolokanov@users.noreply.github.com>
2023-05-19 10:16:28 +0300

    сделать билд

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,18 +1,13 @@
 const CONFIG = {
-  BASE_URL: "http://localhost:3000",
-  // "https://gmkvid.nomoredomains.work",
+  BASE_URL:
+    // "http://localhost:3000",
+    "https://gmkvid.nomoredomains.work",
   ALL_FILMS_URL: "https://api.nomoreparties.co/beatfilm-movies",
   IMG_URL: "https://api.nomoreparties.co/",
 };
+const SHORT_FILM_DURATION = 40;
 const OPEN_DESKTOP = 3;
 const OPEN_TABLET = 2;
 const OPEN_MOBILE = 2;
-const SHORT_FILM_DURATION = 40;
 
-export {
-  CONFIG,
-  OPEN_DESKTOP,
-  OPEN_TABLET,
-  OPEN_MOBILE,
-  SHORT_FILM_DURATION,
-  };
+export { CONFIG, OPEN_DESKTOP, OPEN_TABLET, OPEN_MOBILE, SHORT_FILM_DURATION };
